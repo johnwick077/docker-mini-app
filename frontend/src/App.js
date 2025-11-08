8c4d70d9330498e5594839ee8cbab71992c0b16e
johnwick077 <joelmathew681@gmail.com>
2025-11-08 20:16:37 +0530

    add render link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/hello')
+    fetch("https://docker-mini-backend-381y.onrender.com/api/hello")
       .then(res => res.json())
       .then(data => setMessage(data.message))
       .catch(err => console.error('Error fetching message:', err));
